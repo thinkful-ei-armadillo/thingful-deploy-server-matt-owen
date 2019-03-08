0a1d0e6ddb45617ec72bb95228cbaf386c5abadf
owencyr <cyroc@dukes.jmu.edu>
2019-03-08 15:44:50 -0500

    first commit for deployment

---
+++ src/bcrypt.js
@@ -0,0 +1,8 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+/* 'password' 'bo-password' 'charlie-password' 'sam-password' 'lex-password' 'ping-password' */
+bcrypt.hash('bo-password', 12).then(hash => console.log({ hash }));
+bcrypt.hash('charlie-password', 12).then(hash => console.log({ hash }));
+bcrypt.hash('sam-password', 12).then(hash => console.log({ hash }));
+bcrypt.hash('lex-password', 12).then(hash => console.log({ hash }));
+bcrypt.hash('ping-password', 12).then(hash => console.log({ hash }));
---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,44 @@
+'use strict';
+
+//const bcrypt = require('bcryptjs');
+const AuthService = require('../auth/auth-service');
+
+function requireAuth(req, res, next) {
+  const authToken = req.get('Authorization') || '';
+  let bearerToken;
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({ error: 'Missing bearer token' });
+  } else {
+    bearerToken = authToken.slice(7, authToken.length);
+  }
+  const [tokenUserName, tokenPassword] = Buffer.from(bearerToken, 'base64')
+    .toString()
+    .split(':');
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized Request' });
+  }
+
+  req.app.get('db')('thingful_users')
+    .where({user_name: tokenUserName})
+    .first()
+    .then(user => {
+      if(!user) {
+        return res.status(401).json({error: 'Unathorized request'});
+      }
+      return AuthService.comparePasswords(tokenPassword, user.password)
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: 'Unauthorized Request' });
+          }
+          req.user = user;
+          next();
+        });    
+    })
+    .catch(next);
+
+}
+
+module.exports = {
+  requireAuth
+};
